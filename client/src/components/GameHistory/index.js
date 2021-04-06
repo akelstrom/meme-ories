fc1ead00cc8b75b6c3290b86b7e59d5417a0a6b5
jbeedle19 <josh.beedle@gmail.com>
2021-04-06 10:58:27 -0400

    starting on GameHistory component for Dash

--- client/src/components/GameHistory/index.js
+++ client/src/components/GameHistory/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function GameHistory() {
 
+    // If statement for whether the user has played any games or not
+    // If no game history display "No games played yet, grab some friends and start a game now!"
+    //if ()
     return (
-        
-    )
+        <div>
+            <h2>Game History</h2>
+            <p>Check out your game history!</p>
+            <p>Games Played: 10</p>
+            <p>Points Earned: 1,000</p>
+        </div>
+    );
 }
+
+export default GameHistory;
