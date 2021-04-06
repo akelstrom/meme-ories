0cd69bcb614a18805aff71841580295af2d682eb
steftinsley <69828366+steftinsley@users.noreply.github.com>
2021-04-06 18:51:43 -0500

    added auth to login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Auth from '../utils/auth';
+import { useMutation } from '@apollo/react-hooks';
+import { LOGIN_USER } from '../utils/mutations';
 
-
-const Login = () /*props*/ => {
-   /* const [formState, setFormState] = useState({ email: '', password: '' });
-    const [login, { error }] = useMutation(LOGIN_USER);
+const Login = props => {
+const [formState, setFormState] = useState({ email: '', password: '' });
+const [login, { error }] = useMutation(LOGIN_USER);
   
     // update state based on form input changes
     const handleChange = event => {
@@ -29,24 +31,35 @@ const Login = () /*props*/ => {
       console.error(e);
     }
   };
-  */
+  
     return (
         <section className='login-page'>
         <h1 className='contact-header' data-testid="h1tag">Login here!</h1>
-        <form id="contact-form">
+        <form onSubmit={handleFormSubmit}>
           <div className='contact-div'>
             <label className="contact-label" htmlFor="email">Email:</label><br/>
-            <input type="email" name="email"/>
+            <input type="email" 
+            name="email"
+            placeholder="Your email"
+            value={formState.email}
+            onChange={handleChange}
+            />
           </div>
           <div className='contact-div'>
           <label className="contact-label" htmlFor="password">Password:</label><br/>
-            <input type="password" name="password"/>
+            <input 
+            type="password"
+            placeholder="******"
+             name="password"
+             value={formState.password}
+            onChange={handleChange}
+            />
           </div>
           <div className='contact-div'>
           <button>Submit</button>
           </div>
         </form>
-    
+        {error && <div>Login failed</div>}
         
       </section>
     );
