5341d30747a48047317c8a8334ac2e3fe88e03bb
jbeedle19 <josh.beedle@gmail.com>
2021-04-10 14:25:51 -0400

    removed unused imports from pages/components

--- client/src/components/UserList/index.js
+++ client/src/components/UserList/index.js
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react';
 import { Container, Grid } from '@material-ui/core';
 import UserItem from '../UserItem';
-import { useQuery, useMutation } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/react-hooks';
 import { useDispatch, useSelector } from 'react-redux';
 import { UPDATE_USERS } from '../../utils/actions';
 import { QUERY_USERS } from '../../utils/queries';
-import { ADD_FRIEND } from '../../utils/mutations';
-
 
 const UserList = () => {
     const dispatch = useDispatch();
--- client/src/pages/AddFriends.js
+++ client/src/pages/AddFriends.js
@@ -1,11 +1,4 @@
 import React from 'react'
-import { Container, Grid, Card, CardHeader, CardMedia, CardContent, Button } from '@material-ui/core'
-/*DATA*/
-import friendsData from '../utils/friendsData';
-/*ICONS*/
-
-import PersonAddIcon from '@material-ui/icons/PersonAdd';
-import FaceIcon from '@material-ui/icons/Face';
 import UserList from '../components/UserList';
 
 const AddFriends = () => {
